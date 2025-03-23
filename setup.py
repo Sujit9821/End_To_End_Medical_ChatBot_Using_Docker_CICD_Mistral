68f33d85499af4506b9f88ae1467e9513f576288
sujit <sks.official9821@gmail.com>
2025-03-23 20:11:04 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Sujit Kumar Sangramsingh',
+    author_email= 'sks.official9821@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
